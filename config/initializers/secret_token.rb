92c86663cab8eee118337c772d241b1566cbc204
wadror <wadror@wadror-VirtualBox.(none)>
2015-01-18 18:43:54 +0200

    updated Gemfile for Heroku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ratebeer::Application.config.secret_key_base = '8762532560a491790e78c8c39bffcbd05f5f01654e4e75b15fe5cce7397403aa29e5b45fae98ce383baddd89b626d2191f8f28874c1dbb27d088ce680ecab012'
